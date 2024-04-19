a2da0be6b398cc7fbe7eb9843f4c5a20f5358927
LiamBloomDev <liam@liambloom.me>
2024-04-19 13:44:29 -0400

    added logging

--- app.py
+++ app.py
@@ -28,7 +28,8 @@ from __future__ import annotations
 from typing import Dict, List, Optional, Tuple, Union
 from http import HTTPStatus, HTTPMethod
 from pydantic_settings import BaseSettings, SettingsConfigDict
-from pydantic import SecretStr, HttpUrl
+from pydantic import SecretStr, HttpUrl, Path
+import logging
 import requests
 import json
 
@@ -37,6 +38,10 @@ class Settings(BaseSettings):
     """A class to load strongly typed settings from ./.env"""
 
     model_config = SettingsConfigDict(env_file=".env")
+
+    # Logging
+    LOG_LEVEL: Union[int, str] = logging.INFO
+    LOG_FILE: Optional[Path] = None
 
     # Zoom authentication requirements
     # https://developers.zoom.us/docs/internal-apps/s2s-oauth/
@@ -98,6 +103,17 @@ class Settings(BaseSettings):
 #   Load settings from ./.env
 settings = Settings()
 
+#   Configure logging
+log = logging.getLogger(__name__)
+log.setLevel(settings.LOG_LEVEL)
+if settings.LOG_FILE:
+    handler = logging.FileHandler(settings.LOG_FILE)
+else:
+    handler = logging.StreamHandler("sys.stdout")
+for h in log.handlers:
+    log.removeHandler(h)
+log.addHandler(handler)
+
 
 def auth(
     url: str = str(settings.ZOOM_AUTH_URL),
@@ -117,6 +133,7 @@ def auth(
         - 'grant_type': 'account_credentials'
         - 'account_id': '`ZOOM_ACCOUNT_ID`'
     """
+    log.debug("Making auth request to %s", url)
     resp = requests.post(
         url=url,
         auth=(username, password),
@@ -126,7 +143,14 @@ def auth(
         },
         headers=headers,
     )
-    data = resp.json()
+    log.debug("Auth request status: %s", resp.status_code)
+    log.debug("Auth request message: %s", resp.reason)
+    try:
+        data = resp.json()
+        log.debug("Auth response has data.")
+    except json.JSONDecodeError:
+        log.error("Auth response has no data.")
+        return None, None
     return (data.get("access_token", None), data.get("expires_in", None))
 
 
@@ -146,24 +170,40 @@ def do_get(
     returned in the response.  This token is used to request the next
     page.
     """
+    log.debug("Making GET request to: %s", url)
+    log.debug("Query string: %s", query)
+    log.debug("Combined URL: %s%s", url, query)
     results = []
     if auth:
+        log.debug("Adding auth header to headers.")
         headers.update(auth)
     elif "Authorization" not in headers:
+        log.error("Did not receive an authentication form (header or parameter)!")
         raise Exception("Authorization header missing from GET request.")
     if next_page:
+        log.debug("Adding next_page_token to query string")
         query += f"&next_page_token={next_page}"
+        log.debug("New URL: %s%s", url, query)
     resp = requests.get(
         url=f"{url}{query}",
         headers=headers,
     )
     if resp.status_code != HTTPStatus.OK:
+        log.error("Non-200 response!")
+        log.error("Status code: %s", resp.status_code)
+        log.error("Reason: %s", resp.reason)
         return results
-    results.append(resp.json())
+    try:
+        results.append(resp.json())
+        log.debug("GET response data size: %s", len(results[-1]))
+    except json.JSONDecodeError:
+        log.error("GET response has no data!")
+        return results
     # Try to get a next_page_token from the new result
     next_page = results[-1].get("next_page_token", None)
     # Recursively process all subsequent pages
     if next_page:
+        log.debug("Response has a next page")
         return results.extend(
             do_get(
                 url=url,
@@ -178,17 +218,23 @@ def do_get(
 
 
 if __name__ == "__main__":
+    log.info(">>>   Zoom Phone Number Script    <<<")
+    log.info("Retrieved settings from %s.", settings.model_config.env_file)
+    log.debug("Settings dump:\n%s", settings.model_dump())
     # Holds userID -> phone_numbers
     user_phone_map: Dict[str, List[str]] = {}
     # Holds userID -> extension
     user_extension_map: Dict[str, str] = {}
 
     # Get an access token and store the expiration time (not used)
+    log.info("Attempting authentication to Zoom...")
     t_token, t_exp = auth()
 
     # If a token was not returned, the script cannot continue.
     if not t_token:
+        log.error("Authentication failed!")
         raise Exception("Unable to authenticate to Zoom.")
+    log.info("Authenticated to Zoom.")
 
     # Get all active Zoom Phone user objects.
     all_users_responses = do_get(
@@ -206,7 +252,9 @@ if __name__ == "__main__":
         query="?page_size=100&type=unassigned",
     )
 
-    # Condense the paged results (lists of objects) into a single list of objects
+    log.debug("Processing numbers responses:\n%s", all_phone_responses)
+    # Condense the paged results (lists of objects) into
+    # a single list of objects
     phone_numbers_list: List[Dict] = []
     for response in all_phone_responses:
         phone_numbers_list.extend(response.get("phone_numbers", []))
@@ -214,10 +262,10 @@ if __name__ == "__main__":
     # Extract the phone numbers from the responses
     # https://developers.zoom.us/docs/api/rest/reference/phone/methods/#operation/listAccountPhoneNumbers
     all_phone_numbers = [
-        phone_number.get("number", "ERROR")
-        for phone_number in phone_numbers_list
+        phone_number.get("number", "ERROR") for phone_number in phone_numbers_list
     ]
 
+    log.debug("Processing user responses:\n%s", all_users_responses)
     # Extract the phone numbers and extension from the responses
     # https://developers.zoom.us/docs/api/rest/reference/phone/methods/#operation/listPhoneUsers
     for user_response in all_users_responses:
